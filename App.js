5cbfd9731eeb20e10a9d5b1073b81c4d90a0ec22
Abhishek Kumar <ak7662@gmail.com>
2022-04-09 13:32:01 +0530

    using StyleSheet for css

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another Ashutosh Kumar</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!!</Text>
+      <Button title={'Submit'} />
     </View>
   );
 }
@@ -17,4 +19,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'purple',
+    padding: 16,
+    backgroundColor: 'yellow',
+    borderRadius: 10
+  }
 });
