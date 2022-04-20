1486f0847cad7ce7296e9eb014e8733ba09d1ad0
Abhishek Kumar <ak7662@gmail.com>
2022-04-20 10:41:10 +0530

    a simple flex layout

--- App.js
+++ App.js
@@ -2,14 +2,45 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your Course Goal' />
-        <Button title='Add Goal' />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+      }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+
+        }}>
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>List of Goals</Text>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+        <Text>2</Text>
       </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+
+        }}>
+        <Text>3</Text>
+      </View>
+
     </View>
   );
 }
