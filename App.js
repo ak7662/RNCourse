bc096196e00578a13550b1e49c29a774638dafbe
Abhishek Kumar <ak7662@gmail.com>
2022-04-20 14:12:57 +0530

    adding the items in list

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [enteredGoalArray, setEnteredGoalArray] = useState([])
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  }
+  function addGoalHandler() {
+    setEnteredGoalArray(prevState => [...prevState, enteredGoalText])
+    console.log(enteredGoalArray)
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your Course Goal' />
-        <Button title='Add Goal' />
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your Course Goal'
+          onChangeText={goalInputHandler}
+        />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of Goals</Text>
+        {enteredGoalArray.map((goal, id) =>
+          <Text key={id}>{goal}</Text>
+        )}
+
       </View>
     </View>
   );
