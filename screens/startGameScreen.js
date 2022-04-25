dd11b4c1515f4a6b3809fc497077049d7eabd587
Abhishek Kumar <ak7662@gmail.com>
2022-04-25 12:28:48 +0530

    customized size of button equally

--- screens/startGameScreen.js
+++ screens/startGameScreen.js
@@ -4,9 +4,22 @@ import PrimaryButton from "../components/primaryButton"
 export default function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
+
         </View>
     )
 }
@@ -18,16 +31,24 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         padding: 16,
         marginTop: 100,
-        backgroundColor: '#72063c',
+        backgroundColor: '#92063c',
         elevation: 100,
         shadowColor: 'yellow',
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
-        shadowOpacity: .8
+        shadowOpacity: .8,
+        alignItems: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        flex: 1
     },
     numberInput: {
         height: 50,
-        width: 50,
+        width: 38,
         textAlign: 'center',
         fontSize: 32,
         borderBottomColor: '#ddb52f',
