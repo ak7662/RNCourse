6cb4b469e89958d159af3694d7af83a709e4bee3
Abhishek Kumar <ak7662@gmail.com>
2022-04-28 17:33:15 +0530

    changes in status bar

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import colors from './constants/colors';
 import GameOverScreen from './screens/gameOverScreen';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState('')
@@ -51,20 +52,22 @@ export default function App() {
 
 
   return (
-    <LinearGradient colors={[colors.primary500, colors.accent500]} style={styles.rootScreen}>
-      <ImageBackground
-        source={require('./assets/images/background.png')}
-        resizeMode="cover"
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaView style={styles.rootScreen}>
-          {screen}
-        </SafeAreaView>
+    <>
+      <StatusBar style='light' />
+      <LinearGradient colors={[colors.primary500, colors.accent500]} style={styles.rootScreen}>
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>
+            {screen}
+          </SafeAreaView>
 
-      </ImageBackground>
-    </LinearGradient>
-
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
